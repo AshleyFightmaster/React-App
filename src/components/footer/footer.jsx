bf6b9ae37354170e59b34e8b6be3bfce0dc9930c
Ashley Fightmaster <115577226+AshleyFightmaster@users.noreply.github.com>
2022-12-21 15:29:59 -0700

    first commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './foot.css'
+
+export default class Footer extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    }
+
+    render() {
+    
+
+        return (
+            <div className='Footer'>
+                <footer>
+                    <p>This is my footer.</p>
+                </footer>
+            </div>
+        )
+    }
+}
